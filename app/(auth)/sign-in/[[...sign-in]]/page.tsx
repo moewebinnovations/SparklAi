2701be53b0bf9cffd130a3a4ff47c37f7770aa56
moewebinnovations <moshlogins@gmail.com>
2024-07-01 17:29:39 -0700

    Your commit message

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,16 @@
+import Header from "@/components/Header";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
+  const homeNav = [
+    { name: 'Home', href: '/' },
+  ];
   return (
+    <div>
+      <Header navigation={homeNav}/>
     <div className="flex items-center justify-center h-screen">
       <SignIn fallbackRedirectUrl="/dashboard" />
+    </div>
     </div>
   );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,17 @@
+import Header from "@/components/Header";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
+  const homeNav = [
+    { name: 'Home', href: '/' },
+  ];
   return (
+    <div>
+            <Header navigation={homeNav}/>
     <div className="flex items-center justify-center h-screen">
       <SignUp fallbackRedirectUrl="/dashboard" />
     </div>
+    </div>
+
   );
 }
