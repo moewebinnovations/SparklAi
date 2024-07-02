9d0f2b0326b6e2d8b611e1f21ff96c868d52330c
moewebinnovations <moshlogins@gmail.com>
2024-07-01 17:15:30 -0700

    Your commit message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,16 +13,13 @@
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                    <a href="/about-us" className="hover:underline me-4 md:me-6">About</a>
                 </li>
                 <li>
                     <a href="/privacy-policy" className="hover:underline me-4 md:me-6">Privacy Policy</a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                    <a href="/contact-us" className="hover:underline">Contact</a>
                 </li>
             </ul>
         </div>
